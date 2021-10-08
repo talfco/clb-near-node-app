9f521980da14b391faeda7f1494db62f755a642a
Felix Kuestahler <felix@cloudburo.net>
2021-10-08 08:25:44 +0200

    Working protoype: Store on Web3

--- clb-near-backend/server.js
+++ clb-near-backend/server.js
@@ -1,5 +1,6 @@
 require('global');
 require('dotenv').config();
+
 const process = require('process');
 const minimist = require('minimist');
 const { Web3Storage, File } = require('web3.storage');
@@ -9,15 +10,26 @@ const app = express();
 const multer = require('multer');
 const cors = require('cors');
 const morgan = require('morgan');
+const btoa = require('btoa');
+const atob = require('atob');
+
 
 const crypto = require("crypto");
 const base64 = require('base-64');
 const fs = require("fs");
 
-const wasmAdaptor = import('./dist/index.js');
+// Rust WASM Code Integration
+const wasmInit = import('./dist/index.js');
+// you should always use the memory bindings provided by wasm-pack, and so, implicitly importing the memory allocation
+// in the app.js
+// https://www.reddit.com/r/rust/comments/dgjfn4/how_to_consume_a_uint8array_of_rust_pointer_in/
+//
+//const memory  = import('./dist/index_bg');
 
 app.use(cors())
 app.use(morgan('dev'))
+
+
 
 let storage = multer.memoryStorage({
     destination: function (req, file, cb) {
@@ -30,6 +42,13 @@ let storage = multer.memoryStorage({
 
 let upload = multer({ storage: storage }).array('file')
 
+// Startup the App
+app.listen(8000, function() {
+    console.log('App running on port 8000');
+    uploadToWeb3Store("Hello World","Test.txt",2);
+});
+
+// The REST interface
 app.get('/',function(req,res){
     return res.send('Hello Server')
 })
@@ -46,7 +65,7 @@ app.post('/upload',function(req, res) {
         console.log(req.files);
         console.log(req.files[0].buffer.toString());
 
-        web3store(req.files[0].buffer.toString(), req.files[0].originalname).then(r => function(value) {
+        uploadToWeb3Store(req.files[0].buffer.toString(), req.files[0].originalname, 2).then(r => function(value) {
             console.log("Sucess:"+value); // Success!
         }, function(reason) {
             console.log(reason); // Error!
@@ -57,10 +76,7 @@ app.post('/upload',function(req, res) {
     })
 });
 
-app.listen(8000, function() {
-    console.log('App running on port 8000');
-    uploadToWeb3Store("Hello World");
-});
+// Functions
 
 function hexToBytes(hex) {
     for (var bytes = [], c = 0; c < hex.length; c += 2)
@@ -95,30 +111,20 @@ function doEncrypt(message) {
 }
 
 
-
-function web3store(fileContent, fileName) {
-    const storage = new Web3Storage({token: process.env.WEB3_STORAGE_TOKEN} );
-    let cids = [];
-    let shares = doSSSAEncrypt(fileContent,fileName);
-    for (let index in shares) {
-        console.log('Content "+index+ " added with CID:', cid);
-        cids[index]=cid;
-    }
-    return cids;
-}
-
 async function persistToIPFStore(file) {
     const storage = new Web3Storage({token: process.env.WEB3_STORAGE_TOKEN} );
     const cid = await storage.put([file]);
-    console.log('Content '+file.name+ ' added with CID:'+ cid);
+    console.log('Content '+file.name+ ' added with CID:'+ cid)
+    return cid;
 }
 
-async function uploadToWeb3Store(fileContent) {
-    wasmAdaptor
-        .then(wasm => {
+async function uploadToWeb3Store(fileContent, fileName, sharesLen) {
+    wasmInit
+        .then(async wasm => {
             // First part: Apply Shamir Secrets to the file content
-            const result = wasm.do_shamir_secrets(fileContent);
-            console.log("Number of shares: "+result[0]);
+            const result = wasm.do_shamir_secrets(fileContent,sharesLen);
+            console.log("Number of shares: "+result[0]+" length: "+result.byteLength);
+            var cids = [];
             var startPos = result[0]+1;
             var endPos = result[0]+1;
             // Split the returned Uint8Array into the secret shares
@@ -126,13 +132,68 @@ async function uploadToWeb3Store(fileContent) {
             {
                 endPos += result[i];
                 var share = result.slice(startPos,endPos);
-                console.log("Share: "+i+"/"+result[i]+" reading bytes "+startPos+".."+endPos+" -> "+share);
+                var b64encoded = Buffer.from(share).toString('base64')
+                console.log("Share: "+i+"/"+result[i]+" reading bytes "+startPos+".."+endPos+" Array-Length "+share.length+" -> "+share);
                 try {
-                    persistToIPFStore(new File(share,'share-'+i+'.txt'));
+                    const cid =  await persistToIPFStore(new File(b64encoded,'share'+i+'-'+fileName));
+                    cids[i-1] = cid;
                 } catch (err) {
                     console.error(err)
                 }
                 startPos = endPos;
             }
+            // Let's try to retrieve
+            downloadFromWeb3Store(wasm, cids,sharesLen);
         });
+}
+
+async function downloadFromWeb3Store(wasm, cids, sharesLen) {
+    // The following snippet creates a new WebAssembly Memory instance with an initial size of 10 pages (640KiB),
+    // and a maximum size of 100 pages (6.4MiB)
+    let memory = new WebAssembly.Memory({initial:10, maximum:100});
+    let sharesBytes = new Uint8Array(memory.buffer);
+    sharesBytes[0]=sharesLen;
+    let headerIndex= 1;
+    let sharesBytesIndex = 1+sharesLen;
+
+    const client = new Web3Storage({token: process.env.WEB3_STORAGE_TOKEN} );
+    
+    // Number of shares persisted in the Web3 Store
+    for (let i=0; i<cids.length; i++) {
+        const cid = cids[i];
+        
+        const res = await client.get(cid)
+        console.log(`Web3Store Fetch response! [${res.status}] ${res.statusText}`)
+        if (!res.ok) {
+            throw new Error(`failed to get ${cid} - [${res.status}] ${res.statusText}`)
+        }
+        // Missing prepend, length of each share
+        // unpack File objects from the response
+        const files = await res.files()
+        for (const file of files) {
+            console.log(`${file.cid} -- ${file.size}`);
+            const b64Encoded = await file.arrayBuffer();
+            const buf = new Buffer(new TextDecoder().decode(b64Encoded),'base64');
+            // Adding the File Length in the Header Part
+            console.log("Webstore File ("+headerIndex+") Length: "+buf.length);
+            sharesBytes[headerIndex] = buf.length
+            headerIndex++;
+            // Adding the File Content
+            console.log("Got String: "+String.fromCharCode.apply(null, new Uint8Array(buf)));
+            for (let j=0; j < buf.byteLength; j++) {
+                sharesBytes[sharesBytesIndex] = buf[j];
+                sharesBytesIndex++;
+            }
+            // sharesBytes = concatTypedArrays(sharesBytes,buf);
+        }
+    }
+    const secretStr = wasm.recover_shamir_secrets(sharesBytes);
+    console.log(secretStr);
+}
+
+function concatTypedArrays( buffer1, buffer2) {
+    var tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+    tmp.set(new Uint8Array(buffer1), 0);
+    tmp.set(new Uint8Array( buffer2), buffer1.byteLength);
+    return tmp.buffer;
 }
--- clb-near-backend/src/lib.rs
+++ clb-near-backend/src/lib.rs
@@ -3,22 +3,69 @@ extern crate wasm_bindgen;
 extern crate console_error_panic_hook;
 extern crate sharks;
 extern crate js_sys;
+extern crate core;
 
 use wasm_bindgen::prelude::*;
-
-use std::format;
 use sharks::{ Sharks, Share };
+use core::convert::TryFrom;
+use js_sys::JsString;
 
+// The wasm-pack uses wasm-bindgen to build and generate JavaScript binding file.
+// Import the wasm-bindgen crate.
+use wasm_bindgen::prelude::*;
+
+
+// // https://wasmbyexample.dev/examples/webassembly-linear-memory/webassembly-linear-memory.rust.en-us.html
+// Create a static mutable byte buffer.
+// We will use for passing memory between js and wasm.
+// NOTE: global `static mut` means we will have "unsafe" code
+// but for passing memory between js and wasm should be fine.
+const WASM_MEMORY_BUFFER_SIZE: usize = 2;
+static mut WASM_MEMORY_BUFFER: [u8; WASM_MEMORY_BUFFER_SIZE] = [0; WASM_MEMORY_BUFFER_SIZE];
+
+// Function to store the passed value at index 0,
+// in our buffer
+#[wasm_bindgen]
+pub fn store_value_in_wasm_memory_buffer_index_zero(value: u8) {
+    unsafe {
+        WASM_MEMORY_BUFFER[0] = value;
+    }
+}
+
+// Function to return a pointer to our buffer
+// in wasm memory
+#[wasm_bindgen]
+pub fn get_wasm_memory_buffer_pointer() -> *const u8 {
+    let pointer: *const u8;
+    unsafe {
+        pointer = WASM_MEMORY_BUFFER.as_ptr();
+    }
+
+    return pointer;
+}
+
+// Function to read from index 1 of our buffer
+// And return the value at the index
+#[wasm_bindgen]
+pub fn read_wasm_memory_buffer_and_return_index_one() -> u8 {
+    let value: u8;
+    unsafe {
+        value = WASM_MEMORY_BUFFER[1];
+    }
+    return value;
+}
 
 #[wasm_bindgen]
-pub fn do_shamir_secrets(secret_str : &str) -> js_sys::Uint8Array {
+pub fn do_shamir_secrets(secret_str : &str, size: u8) -> js_sys::Uint8Array {
     // Set a minimum threshold of 10 shares
     console_error_panic_hook::set_once();
-    let shares_size:usize = 10;
+    let shares_size:usize = size.into();
     web_sys::console::log_2(&"Shamirs secret using shares: ".into(),&JsValue::from(shares_size));
     let sharks = Sharks(shares_size as u8);
     let secret_vec = secret_str.as_bytes().to_vec();
+    // We have up to 256 Shares
     let dealer = sharks.dealer(&secret_vec);
+    // We take a subset of the Shares
     let shares: Vec<Share> = dealer.take(shares_size).collect();
 
     // Recover the original secret!
@@ -29,9 +76,9 @@ pub fn do_shamir_secrets(secret_str : &str) -> js_sys::Uint8Array {
     let mut header_vec = Vec::new();
     // First byte is the number of shares
     header_vec.push(shares_size as u8);
+
     // The content vector
     let mut content_vec:Vec<u8> = Vec::new();
-
     for share  in shares {
         let mut share_content:Vec<u8> = Vec::from(&share);
         header_vec.push(share_content.len() as u8);
@@ -40,12 +87,40 @@ pub fn do_shamir_secrets(secret_str : &str) -> js_sys::Uint8Array {
     }
     header_vec.append(& mut content_vec);
     //let s = String::from_utf8(secret_vec);
-    web_sys::console::log_2(&"Returning Shamirs base vector: ".into(),&JsValue::from(header_vec.len()) );
+    web_sys::console::log_2(&"Returning Shamirs base vector (incl. header): ".into(),&JsValue::from(header_vec.len()) );
     js_sys::Uint8Array::from(&header_vec[..])
 }
 
+// https://stackoverflow.com/questions/60171056/wasm-bindgen-arrays-of-u8-as-inputs-and-outputs-generated-javascript-has-diffe
 #[wasm_bindgen]
-pub fn recover_shamir_secrets(share_size:u8,shares_vec : js_sys::Uint8Array) -> String {
-    "Not implemented".into()
+pub fn recover_shamir_secrets(shares_vec : Box<[u8]>) -> String {
+    web_sys::console::log_2(&"WASM: Array Size: ".into(),&JsValue::from(shares_vec.len()));
+    let shares_size: usize = shares_vec[0] as usize;
+    let mut start_pos:usize = (shares_vec[0]+1) as usize;
+    let mut end_pos: usize = (shares_vec[0]+1) as usize;
+    let mut shares_bytes:Vec<Vec<&[u8]>> = Vec::new();
+    let mut shares: Vec<Share> = Vec::new();
+
+    let sharks = Sharks(shares_size as u8);
+    // Header extraction
+    web_sys::console::log_2(&"WASM: Got shares: ".into(),&JsValue::from(shares_size));
+
+    for i in 1..=shares_size
+    {
+        end_pos +=   shares_vec[i] as usize;
+        web_sys::console::log_3(&"Share: reading: ".into(), &JsValue::from(start_pos), &JsValue::from(end_pos));
+        shares.push(Share::try_from(&shares_vec[start_pos ..end_pos]).unwrap());
+        start_pos = end_pos;
+    }
+    let secret :Vec<u8>= sharks.recover(&shares).unwrap();
+    let secret_str = String::from_utf8(secret).unwrap();
+    web_sys::console::log_2(&"The share ".into(), &JsValue::from(&secret_str));
+
+    /*let shares: Vec<Share> = shares_bytes.iter().map(|s| Share::try_from(s.as_slice()).unwrap()).collect();
+    let secret :Vec<u8>= sharks.recover(&shares).unwrap();
+    let secret_str = String::from_utf8(secret).unwrap();
+    web_sys::console::log_2(&"The share ".into(), &JsValue::from(&secret_str));
+    secret_str */
+    "dump".into()
 }
 
